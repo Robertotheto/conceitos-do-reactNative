5c706cb98b93a6c239cbca2679700706eb6f2b1c
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2022-04-30 15:53:33 -0400

    Desafios react native conceitos.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,25 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }));
+    const taskToUpdate = updatedTasks.find(task => task.id === id);
+    if (taskToUpdate) {
+      taskToUpdate.done = !taskToUpdate.done;
+    }
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
@@ -26,10 +36,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
